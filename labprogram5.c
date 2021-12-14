7432250f57dbc190d560df80b4f011a0016f8327
Pavanyamanuru-30 <94230038+Pavanyamanuru-30@users.noreply.github.com>
2021-12-14 15:37:34 +0530

    Update labprogram5.c

--- labprogram5.c
+++ labprogram5.c
@@ -1,7 +1,3 @@
-/*Design, Develop and Implement a Program in C for the following Stack Applications
-a. Evaluation of Suffix expression with single digit operands and operators: +, -, *, /, %, ^ 
-b. Solving Tower of Hanoi problem with n disks
-*/
 // Evaluation of Suffix Expression
 #include <stdio.h>
 #include <string.h>
